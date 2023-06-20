c88148efab1d3038e37a533fe350dd68ea764089
Chetan0402 <chetansharma67g@gmail.com>
2023-06-20 22:10:32 +0530

    3/?? - TestCalcDistance

--- Planet.java
+++ Planet.java
@@ -25,16 +25,20 @@ public class Planet {
     }
 
     public double calcForceExertedByX(Planet p){
-        double r=Math.sqrt(((this.xxPos-p.xxPos)*(this.xxPos-p.xxPos)) +((this.yyPos-p.yyPos)*(this.yyPos-p.yyPos)));
+        double r=calcDistance(p);
         double net_force= ((6.67 / 1e11)*this.mass*p.mass)/(r*r);
         double dx=Math.abs(this.xxPos-p.xxPos);
         return net_force*(dx/r);
     }
 
     public double calcForceExertedByY(Planet p){
-        double r=Math.sqrt(((this.xxPos-p.xxPos)*(this.xxPos-p.xxPos)) +((this.yyPos-p.yyPos)*(this.yyPos-p.yyPos)));
+        double r=calcDistance(p);
         double net_force= ((6.67 / 1e11)*this.mass*p.mass)/(r*r);
         double dy=Math.abs(this.yyPos-p.yyPos);
         return net_force*(dy/r);
+    }
+
+    public double calcDistance(Planet p){
+        return Math.sqrt(((this.xxPos-p.xxPos)*(this.xxPos-p.xxPos)) +((this.yyPos-p.yyPos)*(this.yyPos-p.yyPos)));
     }
 }
