774488c0bdf9d3dcbb96e4f913cd4695ba27bfb9
Chetan0402 <chetansharma67g@gmail.com>
2023-06-22 21:06:03 +0530

    Project 1C Phase 0 Done

---
+++ Project-1C/src/Deque.java
@@ -0,0 +1,8 @@
+public interface Deque<Generic> {
+    public void printDeque();
+    public Generic getRecursive(int i);
+    public Generic get(int i);
+    public Generic removeFirst();
+    public Generic removeLast();
+
+}
